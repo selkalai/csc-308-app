509b4e5f367eb0527cb75d9916657f9e4494ec83
selkalai <selkalai@calpoly.edu>
2026-08-31 09:48:45 -0700

    finsihe d with steps 6-8 for IA 1

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,27 +1,11 @@
 // src/MyApp.jsx
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-    {
-      name: "Charlie",
-      job: "Janitor"
-    },
-    {
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      name: "Dee",
-      job: "Aspring actress"
-    },
-    {
-      name: "Dennis",
-      job: "Bartender"
-    }
-  ]);
+  const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
@@ -36,8 +20,9 @@ function MyApp() {
       characterData={characters}
       removeCharacter={removeOneCharacter}
     />
+    <Form />
   </div>
-);
+  );
 }
 
 export default MyApp;
