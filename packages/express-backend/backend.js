a9f3582ecac5bbe37cc627dcc67037035bbd06e4
selkalai <selkalai@calpoly.edu>
2026-09-05 13:32:33 -0700

    Added getting users by Id func and path

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -42,6 +42,10 @@ const findUserByName = (name) => {
   );
 };
 
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -54,6 +58,16 @@ app.get("/users", (req, res) => {
     res.send(result);
   } else {
     res.send(users);
+  }
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
   }
 });
 
