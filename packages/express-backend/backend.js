17e7f24078f857dc2ffc511036b2386c299b3e87
selkalai <selkalai@calpoly.edu>
2026-09-05 14:39:36 -0700

    added delete and findUserByNameAndJob

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -45,10 +45,28 @@ const findUserByName = (name) => {
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
 
+
+const findUserByNameAndJob = (name, job) => {
+    return users["users_list"].filter(
+    (user) => user["name"] === name && user["job"] === job
+  );
+}
+
 const addUser = (user) => {
   users["users_list"].push(user);
   return user;
 };
+
+const deleteUserById = (id) => {
+  const index = users["users_list"].findIndex((user) => user["id"] === id);
+  if (index === -1) {
+    return null; 
+  }
+  const deletedUser = users["users_list"][index];
+  users["users_list"] = users["users_list"].filter((user) => user["id"] !== id);
+  return deletedUser;
+};
+
 
 
 app.get("/", (req, res) => {
@@ -57,7 +75,13 @@ app.get("/", (req, res) => {
 
 app.get("/users", (req, res) => {
   const name = req.query.name;
-  if (name != undefined) {
+  const job = req.query.job;
+
+  if (name != undefined && job != undefined) {
+    let result = findUserByNameAndJob(name, job);
+    result = { users_list: result };
+    res.send(result);
+  } else if (name != undefined) {
     let result = findUserByName(name);
     result = { users_list: result };
     res.send(result);
@@ -80,6 +104,16 @@ app.post("/users", (req, res) => {
   const userToAdd = req.body;
   addUser(userToAdd);
   res.send();
+});
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params["id"];
+  const deletedUser = deleteUserById(id);
+  if (deletedUser === null) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(deletedUser);
+  }
 });
 
 app.listen(port, () => {
