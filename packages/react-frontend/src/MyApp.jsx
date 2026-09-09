f82c0c093ac3786af10c82259d00fdebb5d2bf5a
selkalai <selkalai@calpoly.edu>
2026-09-08 18:03:52 -0700

    added submit button and post calls - step 3

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -19,6 +19,18 @@ function MyApp() {
     return promise;
   }
 
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
+    return promise;
+  }
+
   useEffect(() => {
   fetchUsers()
 	  .then((res) => res.json())
@@ -26,8 +38,13 @@ function MyApp() {
 	  .catch((error) => { console.log(error); });
 }, [] );
 
-  function updateList(person) {
-  setCharacters([...characters, person]);}
+  function updateList(person) { 
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      })
+}
 
   return (
   <div className="container">
